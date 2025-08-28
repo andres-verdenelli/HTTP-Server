cc3058b6c4ef27ff7d77bd8dbab506ebbdd7d6b1
Andres Verdenelli <andresverdenelli@icloud.com>
2025-08-28 12:30:43 -0300

    ch2: routing l1: middleware done

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
 const app = express()
 const PORT = 8080
 
+app.use(middlewareLogResponses)
 app.use('/app', express.static('./src/app'))
 
 app.get('/healthz', (req, res) => {
-  res.set('Content-Type', 'text/plain; charset=utf-8')
-  res.send('OK')
-  res.status(200)
+  res.status(200).type('text/plain; charset=utf-8').send('OK')
 })
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
 })
+
+function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  res.on('finish', () => {
+    if (res.statusCode >= 400) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.originalUrl} - Status: ${res.statusCode}`
+      )
+    }
+  })
+  next()
+}

98c32080a2a9b0cc2ec7e96db7d8147e9463ec9d
Andres Verdenelli <andresverdenelli@icloud.com>
2025-08-31 20:05:43 -0300

    CH4: JSON L2:JSON done

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,60 @@
 import express, { NextFunction, Request, Response } from 'express'
 import { config } from './config.js'
+import { createCipheriv } from 'crypto'
 
+//Types
+type Handler = (req: Request, res: Response) => void
+type Middleware = (req: Request, res: Response, next: NextFunction) => void
+
+//Constants
 const app = express()
 const PORT = 8080
 
-app.use(middlewareLogResponses)
-app.use('/app', middlewareMetricsInc)
-app.use('/app', express.static('./src/app'))
-
-app.get('/api/healthz', (req, res) => {
+//Handlers
+const handleHealthz: Handler = (_req, res) => {
   res.status(200).type('text/plain; charset=utf-8').send('OK')
-})
+}
 
-app.get('/admin/metrics', (req, res) => {
+const handleMetrics: Handler = (_req, res) => {
   res.send(`<html>
   <body>
     <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+    <p>Chir˝py has been visited ${config.fileserverHits} times!</p>
   </body>
 </html>`)
-})
+}
 
-app.post('/admin/reset', (req, res) => {
+const handleReset: Handler = (_req, res) => {
   config.fileserverHits = 0
   res.sendStatus(200)
-})
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}/app/`)
-})
+const handleValidateChirp: Handler = (req, res) => {
+  req.setEncoding('utf8')
+  let raw = ''
 
-function middlewareLogResponses(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void {
+  req.on('data', chunk => {
+    raw += chunk
+  })
+
+  req.on('end', () => {
+    try {
+      const parsedBody = JSON.parse(raw)
+      if (!parsedBody.body || typeof parsedBody.body !== 'string') {
+        return res.status(400).json({ error: 'Invalid or missing "body"' })
+      }
+      if (parsedBody.body.length > 140) {
+        return res.status(400).json({ error: 'Chirp is too long' })
+      }
+      return res.status(200).json({ valid: true })
+    } catch {
+      return res.status(400).json({ error: 'Invalid JSON' })
+    }
+  })
+}
+
+//MiddleWares
+const middlewareLogResponses: Middleware = (req, res, next) => {
   res.on('finish', () => {
     if (res.statusCode >= 400) {
       console.log(
@@ -45,11 +65,26 @@ function middlewareLogResponses(
   next()
 }
 
-function middlewareMetricsInc(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void {
+const middlewareMetricsInc: Middleware = (_req, _res, next) => {
   config.fileserverHits++
   next()
 }
+
+//Global Middlewares
+app.use(middlewareLogResponses)
+app.use('/app', middlewareMetricsInc)
+app.use('/app', express.static('./src/app'))
+
+//Routes
+app.get('/api/healthz', handleHealthz)
+
+app.get('/admin/metrics', handleMetrics)
+
+app.post('/admin/reset', handleReset)
+
+app.post('/api/validate_chirp', handleValidateChirp)
+
+//Server Listening
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/app/`)
+})

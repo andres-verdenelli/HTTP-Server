b9463991085e8455b26e984abc3d9d213d82ba9b
Andres Verdenelli <andresverdenelli@icloud.com>
2025-08-31 07:39:21 -0300

    CH3: Architecture L4: Admin Namespace done

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,23 @@ app.get('/api/healthz', (req, res) => {
   res.status(200).type('text/plain; charset=utf-8').send('OK')
 })
 
-app.get('/api/metrics', (req, res) => {
-  res.send(`Hits: ${config.fileserverHits}`)
+app.get('/admin/metrics', (req, res) => {
+  res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`)
 })
 
-app.get('/api/reset', (req, res) => {
+app.get('/admin/reset', (req, res) => {
   config.fileserverHits = 0
-  res.send(`Hits: ${config.fileserverHits}`)
+  res.type('text/plain; charset=utf-8').send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`)
 })
 
 app.listen(PORT, () => {

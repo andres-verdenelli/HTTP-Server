de3540a89b51a84bba996467a6f80fc8469fc674
Andres Verdenelli <andresverdenelli@icloud.com>
2025-08-31 07:31:24 -0300

    ch2: routing l2: api config done

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import express, { NextFunction, Request, Response } from 'express'
+import { config } from './config.js'
 
 const app = express()
 const PORT = 8080
 
 app.use(middlewareLogResponses)
+app.use('/app', middlewareMetricsInc)
 app.use('/app', express.static('./src/app'))
 
-app.get('/healthz', (req, res) => {
+app.get('/api/healthz', (req, res) => {
   res.status(200).type('text/plain; charset=utf-8').send('OK')
 })
 
+app.get('/api/metrics', (req, res) => {
+  res.send(`Hits: ${config.fileserverHits}`)
+})
+
+app.get('/api/reset', (req, res) => {
+  config.fileserverHits = 0
+  res.send(`Hits: ${config.fileserverHits}`)
+})
+
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
+  console.log(`Server is running at http://localhost:${PORT}/app/`)
 })
 
 function middlewareLogResponses(
@@ -26,5 +37,14 @@ function middlewareLogResponses(
       )
     }
   })
+  next()
+}
+
+function middlewareMetricsInc(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  config.fileserverHits++
   next()
 }
